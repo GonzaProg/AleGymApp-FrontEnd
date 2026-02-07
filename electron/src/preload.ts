9ba3675f7c0dc345ba615f1b5426181ee80cc983
GonzaProg <gonvasch@gmail.com>
2026-02-07 17:03:07 -0300

    Primera implementación para producción.

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -1,4 +1,18 @@
-require('./rt/electron-rt');
-//////////////////////////////
-// User Defined Preload scripts below
-console.log('User Preload!');
+import { contextBridge, ipcRenderer } from 'electron';
+
+// Exponemos una API segura llamada "electronAPI" al frontend
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Función para escuchar eventos del main (como 'update_ready')
+  on: (channel: string, func: (...args: any[]) => void) => {
+    // Filtramos canales permitidos por seguridad
+    const validChannels = ['update_ready', 'update_available', 'download_progress'];
+    if (validChannels.includes(channel)) {
+      // Importante: Envolver en una nueva función para evitar fugas de memoria
+      ipcRenderer.on(channel, (_event, ...args) => func(...args));
+    }
+  },
+  // Función para limpiar listeners
+  removeAllListeners: (channel: string) => {
+    ipcRenderer.removeAllListeners(channel);
+  }
+});
