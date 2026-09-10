348e92f027a494d8d91717021548f866323bc13a
GonzaProg <gonvasch@gmail.com>
2026-09-10 19:40:42 -0300

    Correcion appId de com.GymMate.app a com.GMate.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.GymMate.app',
+  appId: 'com.GMate.app',
   appName: 'GymMate',
   webDir: 'dist',
 
